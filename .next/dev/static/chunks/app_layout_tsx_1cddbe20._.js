0e97f0765f89823de25264585b0f98d08ba57853
Aiza Nadeem <an3710828@gmail.com>
2026-02-01 17:21:51 +0500

    projects

--- .next/dev/static/chunks/app_layout_tsx_1cddbe20._.js
+++ .next/dev/static/chunks/app_layout_tsx_1cddbe20._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__579403e1._.css",
-  "static/chunks/node_modules_fc8d1d97._.js",
+  "static/chunks/node_modules_2f650ac3._.js",
   "static/chunks/_46c7e28f._.js"
 ],
     source: "dynamic"
